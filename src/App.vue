fe22f4cdbc9ee6c476439fe09c76c32a485a52e5
auauauau <auauauau@gmail.com>
2025-05-14 22:37:40 -0300

    feito a pokedex

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './components/NavbarComponent.vue'
+import Footer from './components/FooterComponent.vue'
+</script>
+
+
+<template>
+  <div class=" bg-[url(https://news.epopculture.com/wp-content/uploads/2025/02/capa-pokemon.jpg)] bg-cover flex flex-col min-h-screen bg-gray-100">
+    <Navbar />
+    <div class="main container mx-auto px-4 py-6">
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
+</template>
